4eb5bf639666714bbf8d141ee9b6e9dab6ee7450
WI1ngs <qq5391336@gmail.com>
2019-03-28 15:17:06 +0800

    导出功能

--- src/components/DateDemo.vue
+++ src/components/DateDemo.vue
@@ -2,64 +2,31 @@
   <div id="datee">
     <div span="12">
       <div class="dateleft">
-        <DatePicker
-          type="date"
-          :value="date"
-          v-model="dateee"
-          format="yyyy/MM/dd"
-          placeholder="选择日期"
-          id="time"
-          style="width: 200px"
-        ></DatePicker>
-      </div>
-      <div class="dateleft">
         <Select v-model="model1" style="width:200px">
-          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          <Option v-for="item in dateList" :value="item.value" :key="item.value">{{ item.label }}</Option>
         </Select>
       </div>
 
       <div class="dateleft">
         <Button shape="circle" icon="ios-search"></Button>
-        <Button class='dateButton' type="default" icon="ios-swap" @click="changeToDay">切换报表</Button>
+        <Button class='dateButton'  icon="ios-swap" @click="changeToDay">切换报表</Button>
       </div>
 
       <div class="dateright">
-        <Button type="default ">导出</Button>
+        <Button>导出</Button>
       </div>
     </div>
   </div>
 </template>
 <script>
+import axios from 'axios'
+import qs from 'qs'
+
 export default {
   name: "DateDemo",
   data() {
     return {
-      cityList: [
-        {
-          value: "New York",
-          label: "New York"
-        },
-        {
-          value: "London",
-          label: "London"
-        },
-        {
-          value: "Sydney",
-          label: "Sydney"
-        },
-        {
-          value: "Ottawa",
-          label: "Ottawa"
-        },
-        {
-          value: "Paris",
-          label: "Paris"
-        },
-        {
-          value: "Canberra",
-          label: "Canberra"
-        }
-      ],
+      dateList:[],
       date:null,
       model1:null,
       dateee:null
@@ -73,7 +40,20 @@ export default {
   },
   mounted(){
     var myToken = window.localStorage.getItem('token')
+    var myId = this.$store.user
     this.axios.defaults.headers.common['tk-token'] = myToken
+    // debugger
+    this.axios.post('http://10.1.9.54:9200/daily/weeklyinfo/showWeeklyInfo',
+      qs.stringify({
+        userId:myId
+      })
+    )
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.error(err); 
+    })
   }
 }
 </script>
