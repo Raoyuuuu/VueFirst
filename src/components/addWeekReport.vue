52d62439bcb5e9c1d54df296f0681bca4f9963f8
raoyuuuu <raoyuuuu@126.com>
2019-03-29 11:24:48 +0800

    个人周报项

---
+++ src/components/addWeekReport.vue
@@ -0,0 +1,190 @@
+<template>
+    <div>
+        <i-button class = 'saveReport' type="default" @click="addReport">保存表单</i-button>
+        <div class="report">
+            <img src="@/assets/keytopLogo.jpg" class="logo"/>
+            <div class = "operationBody">
+            <i-button class = 'contorlReport' type="default" @click="reSet">重置</i-button>
+            </div>
+            <div id ="smallSizeInput">
+                <div class = 'smallSize'>
+                    项目名称:<i-Input placeholder="" class = 'reportInput' v-model="target"></i-Input>
+                </div>
+                <div class = 'smallSize'>
+                    类别:<i-Input placeholder="" class = 'reportInput' v-model="type"></i-Input>
+                </div>
+                <div class = 'smallSize'>
+                    内容描述:<i-Input placeholder="" class = 'reportInput' v-model="description"></i-Input>
+                </div>
+                <div class = 'smallSize'>
+                    当前进度:<i-Input placeholder="" class = 'reportInput' v-model="progress"></i-Input>
+                </div>
+            </div>
+            <div id ="largeSizeInput">
+                <div class = 'largeSize'>
+                    预计完成时间:<Input  placeholder=""  v-model="planDate"/>
+                </div>
+                <div class = 'largeSize'>
+                    未完成原因:<Input  type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder=""  v-model="reason"/>
+                </div>
+                <!-- <div class = 'largeSize'>
+                    遗留问题:<Input  type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder=""  v-model="problem"/>
+                </div> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import qs from 'qs'
+
+export default {
+    data(){
+        return{
+            userId:null,
+            target:null,
+            type:null,
+            description:null,
+            progress:null,
+            planDate:null,
+            reason:null,
+            //problem:null,
+            time:null,
+        }
+    },
+    methods:{
+        reSet:function(){
+            this.target = null
+            this.type = null
+            this.description = null
+            this.progress = null
+            this.planDate = null
+            this.reason = null
+            //this.problem = null
+        },
+        addReport:function(){
+            this.axios.post('http://10.1.9.53:9200/daily/weeklyinfo/save',
+                qs.stringify({
+                    userId:this.$data.userId,
+                    // date:this.$data.time,
+                    target:this.$data.target,
+                    type:this.$data.type,
+                    description:this.$data.description,
+                    progress:this.$data.progress,
+                    planDate:this.$data.planDate,
+                    reason:this.$data.reason,
+                   // problem:this.$data.problem,
+                })
+            )
+            .then(res => {
+                if(res.data.resultCode == '200'){
+                    alert('操作成功')
+                    this.$router.go(-1)
+                }
+                console.log(res)
+            })
+            .catch(err => {
+                console.error(err); 
+            })
+        }
+    },
+    mounted(){
+        var myToken = window.localStorage.getItem('token')
+        var da = new Date().getTime()
+        da = new Date(da);
+        // var year = da.getFullYear()+'-';
+        // var month = da.getMonth()+1+'-';
+        // var date = da.getDate();
+        // var time = year+month+date
+        this.time = da
+        this. userId = window.localStorage.getItem('userId')
+        console.log(this.$data.time)
+        
+        this.axios.defaults.headers.common['tk-token'] = myToken
+    },
+ 
+}
+</script>
+ 
+<style>
+    .saveReport{
+        position: relative;
+        top: 13px;
+        float: right;
+        height: 30px;
+        width: 80px;
+        margin-right: 30px
+    }
+    .contorlReport{
+        
+        float: right;
+        height: 30px;
+        width: 80px;
+        margin-left: 20px
+    }
+    .reportMessage{
+        padding-top: 10px;
+        height: 41px;
+        width: 100%;
+        position: relative;
+        top: 51px;
+        border: rgba(165, 165, 165, 0.904) 2px solid;
+    }
+    .report{
+        width: 100%;
+        position: relative;
+        height: 364px;
+        top: 49px;
+        border: rgba(165, 165, 165, 0.904) 2px solid;
+    }
+    .reportSelect{
+        height: 30px;
+        width: 21.31%;
+        margin-right: -20px;
+        
+    }
+    .reportInput{
+        height: 30px;
+        width: 80%;
+        margin-right: -20px;
+    }
+    .operationBody{
+        height: 54px;
+        text-align: right;
+        padding-right: 26px;
+        padding-top: 24px
+    }
+    .smallSize{
+        margin-top:24px; 
+        display: block;
+        width: 70%;
+    }
+    .largeSize{
+        margin-top:24px; 
+        display: block;
+        width: 45%;
+        text-align: left
+    }
+    #smallSizeInput{
+        width: 50%;
+        display: table;
+        padding-left:20%;
+        position: relative;
+        top: 61px;
+    }
+    #largeSizeInput{
+        width: 50%;
+        display: table;
+        float: right;
+        padding-left:5%;
+        margin-top: -219px
+    }
+    .logo{
+        position: absolute;
+        width: 299px;
+        height: 85px;
+        top: 12px;
+        left: 23%;
+    }
+</style>
--- src/components/selfWeekyReport.vue
+++ src/components/selfWeekyReport.vue
@@ -1,13 +1,166 @@
 <template>
-    <div>content</div>
+    <div>
+        <!-- <Button type="primary" @click="clickFn">编辑</Button> -->
+    <div class="addButton">
+        <Button type="default" icon="md-add" @click="clickFn" >新增</Button>
+        <!-- <Button type="primary">撤回</Button> -->
+        </div>
+        <Table border :columns="weekyColumns" :data="weekyData"></Table>
+    </div>
+    
 </template>
-
 <script>
-export default {
+import axios from 'axios'
+import qs from 'qs'
 
-}
+    export default {
+        data () {
+            return {
+                weekyColumns: [
+                    {
+                        title: '时间',
+                        align: 'center',
+                        key: 'kind',
+                        width:'150px',
+                        sortable: true,
+                        render:(h,params)=>{
+                            
+                            if(params.row.kind===1){
+                               return h('div','本周')
+                               
+                            }
+                            else{
+                               return h('div','下周')
+                            }
+                        }
+                    },{
+                        title: '项目名称',
+                        align: 'center',
+                        key: 'target'
+                    },{
+                        title: '类别',
+                        align: 'center',
+                        key: 'type',
+                        
+                    },{
+                        title: '内容描述',
+                        align: 'center',
+                        key: 'description'
+                    },{
+                        title: '当前进度',
+                        align: 'center',
+                        key: 'progress'
+                    },{
+                        title: '预计完成时间',
+                        align: 'center',
+                        key: 'planDate'
+                    },{
+                        title: '未完成原因',
+                        align: 'center',
+                        key: 'reason'
+                    },{
+                        title: '操作',
+                        key: 'action',
+                        width: 200,
+                        align: 'center',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'default',
+                                        size: 'small',
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.show(params.index)
+                                        }
+                                    }
+                                }, '编辑'),
+                                h('Button', {
+                                    props: {
+                                        type: 'default',
+                                        size: 'small',
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.remove(params.index)
+                                        }
+                                    }
+                                }, '撤回')
+                            ]);
+                        }
+                    }
+                ],
+                weekyData:[]
+            }
+        },
+        methods: {
+            show (index) {
+                console.log(this.$data.weekyData[index].weekyId)
+                this.axios.post('http://10.1.9.53:9200/daily/weeklyinfo/findByUserId',
+                qs.stringify({
+                    dailyId:this.$data.weekyData[index].weekyId
+                })
+                )
+                .then(res => {
+                    if(res.data.resultCode == '200'){
+                        this.$store.reportContentWeek = res.data.data
+                        this.$router.push('/reportOperation')
+                        }
+                    
+                    console.log(res)
+                })
+                .catch(err => {
+                    console.error(err); 
+                })
+            },
+            remove (index) {
+                this.weekyData.splice(index, 1);
+            },
+            clickFn:function(){
+                this.$router.push('/addWeekReport')
+            }
+        },mounted(){
+            var myToken = window.localStorage.getItem('token')
+            var userId = window.localStorage.getItem('userId')
+            var userName = window.localStorage.getItem('username')
+            this.axios.defaults.headers.common['tk-token'] = myToken
+             this.axios.post('http://10.1.9.53:9200/daily/weeklyinfo/findByUserId',qs.stringify({
+            userId:userId
+        }))
+        .then(res => {
+            console.log(res)
+            if(res.data.resultCode == '200'){
+                // console.log(res.data.data.length)
+                // for (var i=0;i<res.data.data.length;i++){
+                    
+                //     var da = res.data.data[i].date
+                //      da = new Date(da);
+                //      var year = da.getFullYear()+'年';
+                //      var month = da.getMonth()+1+'月';
+                //      var date = da.getDate()+'日';
+                //      var time = year+month+date
+                //      res.data.data[i].date = time
+                // }
+               this.weekyData = res.data.data
+                //  this.$store.title = userName+"的日报表"
+                // console.log(this.$store.title)
+                }
+            })
+            .catch(err => {
+                console.error(err); 
+            })
+        }
+    }
 </script>
-
 <style>
-
+    .addButton{
+        text-align: right;
+        margin-right: 10px;
+        margin-bottom: 10px;
+        margin-top: 10px
+    }
 </style>
