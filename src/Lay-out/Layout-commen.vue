9454514ce775825939e75e283dd7a67eaf8b9901
WI1ngs <qq5391336@gmail.com>
2019-04-04 16:03:31 +0800

    树状菜单

--- src/Lay-out/Layout-commen.vue
+++ src/Lay-out/Layout-commen.vue
@@ -17,16 +17,19 @@
             </Header>
             <Layout>
                 <Sider hide-trigger :style="{background: '#fff'}">
-                    <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-                        <Submenu name="1" >
+                    
+                         
+                <Tree :data="theModel" class="menuTree" :render="renderContent"></Tree>
+                        <!-- <Submenu name="1" >
                             <template slot="title">
                                 <Icon type="ios-navigate"></Icon>
                                 菜单
                             </template>
-                            <MenuItem class="item" v-for="(item,index) in menuItem" :name='index' @click.native="turnToPage(item.menuUrl)" 
+                            <MenuItem class="item" v-for="(item,index) in theModel" :name='index' @click.native="turnToPage(item.menuUrl)" 
                             :key="item.menuId">{{item.menuName}}</MenuItem>
-                        </Submenu>
-                    </Menu>
+                        </Submenu> -->
+
+                    
                 </Sider>
                 <Layout :style="{padding: '0 24px 24px'}">  
                     <Breadcrumb :style="{margin: '24px 0'}" class="tip">   
@@ -46,7 +49,6 @@
 import axios from 'axios'
 import qs from 'qs'
 import statusBar from '@/head/StatusBar'
-
 import ContentBlank from '@/components/reportOperation'
 
 
@@ -54,7 +56,6 @@ import ContentBlank from '@/components/reportOperation'
     export default {
          components:{
              statusBar,
-
              ContentBlank,
             //  ContentBlankWeek
          },
@@ -63,19 +64,70 @@ import ContentBlank from '@/components/reportOperation'
                  msg: this.$store.title,
                  username: window.localStorage.getItem('username'),
                  department:[],
-                 menuItem:[]
+                 theModel:[]
              }
          },
          methods:{
-             turnToPage:function(path){
-                 this.$router.push(path)
+              renderContent (h, { root, node, data }) {
+                return h('span', {
+                    style: {
+                        display: 'inline-block',
+                        width: '94%',
+                        // marginLeft:'10%'
+                    }
+                }, [
+                    h('span', {
+                        style: {
+                            width: '94%',
+                            display: 'inline-block',
+                            textAlign:'left'
+                            }
+                        }, [
+                        h('Button', {
+                                props: Object.assign({}, this.buttonProps, {
+                                    type: 'text',
+                                }),
+                                style: {
+                                    width: '100%',
+                                    textAlign:'center',
+                                    fontSize:'15px',
+                                },
+                                on: {
+                                    click: () => { 
+                                        this.turnToPage(data) 
+                                        }
+                                }
+                            },data.title)
+                        
+                    ]),
+                ]);
+            },
+             turnToPage:function(data){
+                console.log(data)
+                if(!data.children){
+                   this.$router.push(data.menuUrl) 
+                }else if(!data.expand){
+                     this.$set(data,'expand',true)
+                 }else if(data.expand){
+                     this.$set(data,'expand',false)
+                 }
              },
              turnUp:function(){
                  this.$router.go(-1)
              },
              turnIndex:function(){
                  this.$router.push("/welcome")
-             }
+             },
+             setTitle(dataList){
+                var length = dataList.length
+                for(var i=0;i<length;i++){
+                    this.$set(dataList[i],'title',dataList[i].menuName)
+                    if(dataList[i].children){
+                    // console.log('111')
+                    this.setTitle(dataList[i].children)
+                }
+                }
+            }
          },mounted(){
              var myToken = window.localStorage.getItem('token')
              this.$router.push('/welcome')
@@ -90,11 +142,15 @@ import ContentBlank from '@/components/reportOperation'
              .catch(err => {
                  console.error(err); 
              })
-             this.axios.post('http://10.1.9.54:9200//daily/menu/findMenuByUserId')
+             this.axios.post('http://10.1.9.54:9200//daily/menu/findMenuTreeByUserId')
              .then(res => {
-                // console.log(res)
+                console.log(res)
                  if(res.data.resultCode == '200'){
-                     this.menuItem = res.data.data;
+                    var data = []
+                    console.log(res)
+                    data = res.data.data
+                    this.setTitle(data)
+                    this.theModel = data
                  }
              })
              .catch(err => {
@@ -137,5 +193,8 @@ import ContentBlank from '@/components/reportOperation'
 }
 .titleButton{
     margin-left: 50px
+}
+.menuTree{
+    width:100%;
 }
 </style>
--- src/components/menuControl.vue
+++ src/components/menuControl.vue
@@ -1,133 +1,269 @@
 <template>
-    <div >
-      <div class='addButton'>
-        <Button @click="clickFn" type="default" icon="md-add">新增</Button>
-      </div>
-      <Table class='menu' border :columns="menuColumns" :data="menuList"></Table>
+    <div class='treeDiv'>
+    <Modal
+        v-model="modal6"
+        title="Title"
+        @on-ok="append2">
+        菜单名称:<Input prefix="ios-contact" type="text"  style="width: auto" v-model="menuName"/>
+        <br/>
+        菜单链接:<Input class = 'loginpassword' prefix="md-lock" type="text"  style="width: auto" v-model="menuUrl"/>
+        <!-- password:<input type="password" name = "password" v-model="password"/> -->
+        <br/>
+    </Modal>
+    <Modal
+        v-model="modal7"
+        title="Title"
+        @on-ok="upDate">
+        菜单名称:<Input prefix="ios-contact" type="text"  style="width: auto" v-model="menuName"/>
+        <br/>
+        菜单链接:<Input class = 'loginpassword' prefix="md-lock" type="text"  style="width: auto" v-model="menuUrl"/>
+        <!-- password:<input type="password" name = "password" v-model="password"/> -->
+        <br/>
+    </Modal>
+    <Tree :data="data5" :render="renderContent" class="tree" ></Tree>
     </div>
 </template>
+<style>
+ .tree{
+     text-align: left
+ }
+ .treeDiv{
+     margin-left:10%
+ }
+</style>
 
 <script>
 import axios from 'axios'
 import qs from 'qs'
-export default {
-    data () {
+
+var nodeKey
+var dataTemp = []
+var thisMenuId
+    export default {
+        data () {
             return {
-                menuColumns: [
-                    {
-                        type: 'index',
-                        align: 'center',
-                        title:'No'
-                    },{
-                        title: '菜单名',
-                        align: 'center',
-                        key: 'menuName',
-                    },{
-                        title: '菜单路由',
-                        align: 'center',
-                        key: 'menuUrl',
-                    },{
-                        title: '操作',
-                        key: 'action',
-                        align: 'center',
-                        render: (h, params) => {
-                            return h('div', [
-                                h('Button', {
-                                    props: {
-                                        type: 'default',
-                                        size: 'small'
-                                    },
+                data5: [
+                     {
+                        title: '功能菜单',
+                        expand: true,
+                        menuId:'0',
+                        render: (h, { root, node, data }) => {
+                            return h('span', {
+                                style: {
+                                    display: 'inline-block',
+                                    width: '100%'
+                                }
+                            }, [
+                                h('span', {
                                     style: {
-                                        marginRight: '50px'
-                                    },
-                                    on: {
-                                        click: () => {
-                                            this.show(params.index)
+                            width: '120px',
+                            display: 'inline-block',
+                            }
+                        
+                                },[
+                                    h('Icon', {
+                                        props: {
+                                            type: 'ios-folder-outline'
+                                        },
+                                        style: {
+                                            marginRight: '8px'
                                         }
+                                    }),
+                                    h('span', data.title)
+                                ]),
+                                h('span', {
+                                    style: {
+                                        display: 'inline-block',
+                                        // float: 'right',
+                                        marginLeft: '32px'
                                     }
-                                }, '编辑'),
-                                h('Button', {
-                                    props: {
-                                        type: 'default',
-                                        size: 'small'
-                                    },
-                                    on: {
-                                        click: () => {
-                                            this.remove(params.index)
+                                }, [
+                                    h('Button', {
+                                        props: Object.assign({}, this.buttonProps, {
+                                            icon: 'ios-add',
+                                            type: 'primary'
+                                        }),
+                                        style: {
+                                            width: '64px'
+                                        },
+                                        on: {
+                                            click: () => { this.append(data) }
                                         }
-                                    }
-                                }, '删除')
+                                    })
+                                ])
                             ]);
-                        }
+                        },
                     }
                 ],
-                menuList:[]
+                buttonProps: {
+                    type: 'default',
+                    size: 'small',
+                },
+            
+                modal6: false,
+                modal7: false,
+                menuName:null,
+                menuUrl:null
             }
-        },methods:{
-            show(index){
-                var thisMId = this.$data.menuList[index].menuId
-                console.log(thisMId)
-                this.axios.post('http://10.1.9.54:9200/daily/menu/findMenuById',
-                qs.stringify({
-                    menuId:thisMId
-                }))
-                .then(res => {
-                    console.log(res)
-                    if(res.data.resultCode == '200'){
-                    this.$store.menuContent = res.data.data
-                    this.$router.push('/menuOperation')
+        },
+        methods: {
+            renderContent (h, { root, node, data }) {
+                return h('span', {
+                    style: {
+                        display: 'inline-block',
+                        width: '100%',
+                        // marginLeft:'10%'
                     }
+                }, [
+                    h('span', {
+                        style: {
+                            width: '100px',
+                            display: 'inline-block',
+                            }
+                        }, [
+                        h('Icon', {
+                            props: {
+                                type: 'ios-paper-outline'
+                            },
+                            style: {
+                                marginLeft: '8px'
+                            }
+                        }),
+                        h('span', data.title),
+                        
+                    ]),
+                    h('span', {
+                        style: {
+                            display: 'inline-block',
+                            // float: '',
+                            marginLeft: '32px'
+                        }
+                    }, [
+                        h('Button', {
+                            props: Object.assign({}, this.buttonProps, {
+                                icon: 'ios-add'
+                            }),
+                            style: {
+                                marginRight: '8px'
+                            },
+                            on: {
+                                click: () => { this.append(data) }
+                            }
+                        }),
+                        h('Button', {
+                            props: Object.assign({}, this.buttonProps, {
+                                icon: 'ios-remove'
+                            }),
+                            style: {
+                                marginRight: '8px'
+                            },
+                            on: {
+                                click: () => { this.remove(root, node, data) }
+                            }
+                        }),
+                        h('Button', {
+                            props: Object.assign({}, this.buttonProps, {
+                                icon: 'ios-apps'
+                            }),
+                            on: {
+                                click: () => { this.show(data) }
+                            }
+                        })
+                    ])
+                ]);
+            },
+            show(data){
+                this.modal7 = true
+                this.menuName = data.menuName
+                this.menuUrl = data.menuUrl
+                thisMenuId = data.menuId
+                dataTemp = data
+            },
+            upDate(){
+                this.axios.post('http://10.1.9.54:9200/daily/menu/updateMenu',
+                    qs.stringify({
+                        menuId:thisMenuId,
+                        menuName:this.$data.menuName,
+                        menuUrl:this.$data.menuUrl
+                    })
+                )
+                .then(res => {
+                    if(res.data.resultCode == '200'){
+                    this.$set(dataTemp, 'title', this.$data.menuName);
+                    // alert('操作成功')
+
+                }
                 })
                 .catch(err => {
                     console.error(err); 
                 })
+            },
+            append (data) {
+                this.modal6 = true
+                nodeKey = data.nodeKey
+                dataTemp = data   
+            },append2 () {
+                const children = dataTemp.children || [];
+                children.push({
+                    title: this.menuName,
+                    menuPid: dataTemp.menuId,
+                    menuName: this.menuName,
+                    menuUrl: this.menuUrl
+                });
+                this.$set(dataTemp, 'children', children);
+                this.axios.post('http://10.1.9.53:9200/daily/menu/addMenu',
+                    qs.stringify({
+                        menuPid: dataTemp.menuId,
+                        menuName: this.menuName,
+                        menuUrl: this.menuUrl
+                    })
+                )
+                .then(res => {
+                    console.log(res)
+                })
+                .catch(err => {
+                    console.error(err); 
+                })
+        
+                
                 
             },
-             remove(index){
-                var thisMId = this.$data.menuList[index].menuId
-                 this.axios.post('http://10.1.9.54:9200/daily/menu/delMenuById',
-                qs.stringify({
-                    menuId:thisMId
-                }))
-                .then(res => {
-                    console.log(res)
-                    if(res.data.resultCode == '200'){
-                        alert("操作成功")
-                        this.menuList.splice(index, 1)                 
-                    }
-                })
-                .catch(err => {
-                    console.error(err); 
-                })
+            remove (root, node, data) {
+                const parentKey = root.find(el => el === node).parent;
+                const parent = root.find(el => el.nodeKey === parentKey).node;
+                const index = parent.children.indexOf(data);
+                parent.children.splice(index, 1);
             },
-            clickFn:function(){
-                this.$router.push('/addMenu')
+            setTitle(dataList){
+                var length = dataList.length
+                for(var i=0;i<length;i++){
+                    this.$set(dataList[i],'title',dataList[i].menuName)
+                    if(dataList[i].children){
+                    // console.log('111')
+                    this.setTitle(dataList[i].children)
+                }
+                }
+               
+
             }
         },mounted(){
             var myToken = window.localStorage.getItem('token')
-            this.axios.defaults.headers.common['tk-token'] = "myToken"
-            this.axios.post('http://10.1.9.54:9200/daily/menu/findAllMenus')
+            var data = []
+            this.axios.defaults.headers['kt-token'] = myToken
+            this.axios.post('http://10.1.9.53:9200/daily/menu/findMenuTree')
             .then(res => {
                 console.log(res)
-                if(res.data.resultCode == '200'){
-                    this.menuList = res.data.data
-                }
+                data = res.data.data
+                this.setTitle(data)
+                this.$set(this.data5[0],'children',data)
+                // debugger
             })
             .catch(err => {
                 console.error(err); 
             })
+            // this.setTitle(data)
+            // debugger
+            // this.$set(this.data5[2],'title','parent 3')
         }
-}
+    }
 </script>
-    
-<style>
-    .addButton{
-        margin-top: 10px;
-        text-align: right;
-    }
-    .menu{
-        margin-top: 15px;
-        width: 100%; 
-        display: inline-block;
-    }
-</style>
